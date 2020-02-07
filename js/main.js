b3c3d859bc66469b28c6e8063d920ce93711b0ff
Jonathan Zelaya <WhskyRbbt@Jonathans-MacBook-Air.local>
2020-02-06 20:49:52 -0800

    update js

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// The premise of the game is to have the user pick a cell, wjile trying not to pick one with a mine in it.
+
+// It will be a random generated layout for the mines, and one of two things can happen, the user chooses a cell with a mine, or one without. I will need to write an if else statement detailing if there is a mine, if not, then either display a number representing the number of mines in proximity, or clear the cell if the value is 0. 
+
+//I will need to write a function that can iterate through all cells with a value of 0 that are in direct vicinity of the cell chosen, and remove the cell "cover" to reveal all the empty cell until it reaches a cell that has a value of 1 - 8 (max number of mines around a cell.)
+
+//i have to set values for variables (not yet known) then i have to write the process of calculating how many mines are going to be in the area (gameboard).
+
+// I think I can write a for loop for rows, and a separate one for columns, and then call them as a referrence when calculating the mine locations.
+
+// All cells will need to have a 'click' event listener, and then an immeidate function calling the "checkForMine" function that will run the cellChecker function that will check the cells value.
+
+//I have to try and figure out how to randomly generate the placement and amount of bombs using a math.random property, or something, then I need to create a function to remove the box for every empty cell. 
+
+const map = [];
+const mine;
+const player = true; //maybe
+
+function checkForMine () {
+
+}
+
+function cellChecker () {
+    // If / else statement (if empty, do this, else, do that ( then a nested if else (if mine, do this, if number, do that.)))
+}
